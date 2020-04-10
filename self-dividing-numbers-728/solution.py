098db729fe416cd0b2090494d1aa06f1e77b9aca
Kevin Kaspari <kevin.kaspari@gmail.com>
2020-04-10 18:41:04 -0400

    Add break to else block Improve variable naming

--- self-dividing-numbers-728/solution.py
+++ self-dividing-numbers-728/solution.py
@@ -1,24 +1,28 @@
+from typing import List
+
+
 class Solution:
     def selfDividingNumbers(self, left: int, right: int) -> List[int]:
         selfDividingNums = []
 
         while (left <= right):
-            left_string = str(left)
-            result = 0
+            leftString = str(left)
+            negResultCounter = 0
 
-            for ch in left_string:
-                digit = int(ch)
+            for char in leftString:
+                digit = int(char)
 
                 if digit == 0:
-                    result += 1
+                    negResultCounter += 1
                     break
 
                 if (left % digit) == 0:
-                    result += 0
+                    negResultCounter += 0
                 else:
-                    result += 1
+                    negResultCounter += 1
+                    break
 
-            if result == 0:
+            if negResultCounter == 0:
                 selfDividingNums.append(left)
 
             left += 1
