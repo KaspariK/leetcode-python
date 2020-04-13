8972e2bd7a3d68a1d295e3b38a7ed1a9290e3ab5
Kevin Kaspari <KKASPARI@goremutual.ca>
2020-04-13 16:45:05 -0400

    Update best-time-to-buy-and-sell-stocks to handle empty list

--- best-time-to-buy-and-sell-stock-121/solution.py
+++ best-time-to-buy-and-sell-stock-121/solution.py
@@ -1,10 +1,11 @@
+import sys
 from typing import List
 
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         maxProfit = 0
-        minVal = prices[0]
+        minVal = sys.maxsize
 
         for num in prices:
             if num < minVal:
