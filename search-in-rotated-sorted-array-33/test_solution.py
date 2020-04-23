c24d01b27d227e882f6fb7f96ba79581bdba6363
Kevin Kaspari <KKASPARI@goremutual.ca>
2020-04-23 17:23:13 -0400

    Add README for search-in-rotated-sorted-array Add tests for two main cases in search-in-rotated-sorted-array

---
+++ search-in-rotated-sorted-array-33/test_solution.py
@@ -0,0 +1,20 @@
+import unittest
+from solution import Solution
+
+
+class TestSolution(unittest.TestCase):
+    def test_solution_found(self):
+        s = Solution()
+
+        result = s.search([4, 5, 6, 7, 0, 1, 2], 0)
+        self.assertEqual(result, 4)
+
+    def test_solution_not_found(self):
+        s = Solution()
+
+        result = s.search([4, 5, 6, 7, 0, 1, 2], 3)
+        self.assertEqual(result, -1)
+
+
+if __name__ == "__main__":
+    unittest.main()
