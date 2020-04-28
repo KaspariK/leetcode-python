3743fb9ebb0e5fd751f9d7379322389ff35d0e40
Kevin Kaspari <KKASPARI@goremutual.ca>
2020-04-28 17:03:28 -0400

    Add README and test for unique-paths

---
+++ unique-paths-62/test_solution.py
@@ -0,0 +1,14 @@
+import unittest
+from solution import Solution
+
+
+class TestSolution(unittest.TestCase):
+    def test_solution(self):
+        s = Solution()
+
+        result = s.uniquePaths(7, 3)
+        self.assertEqual(result, 28)
+
+
+if __name__ == "__main__":
+    unittest.main()
