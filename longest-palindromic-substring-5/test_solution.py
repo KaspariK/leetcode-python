3f779366169808b9ff0fdb35c0db78aa6ac21c07
Kevin Kaspari <kevin.kaspari@gmail.com>
2020-05-09 20:59:52 -0400

    Add tests for manachers algorithm in longest-palindromic-substring

--- longest-palindromic-substring-5/test_solution.py
+++ longest-palindromic-substring-5/test_solution.py
@@ -33,6 +33,38 @@ class TestSolution(unittest.TestCase):
         result = s.longestPalindrome("abc")
         self.assertEqual(result, "a")
 
+    # Same tests done for Manacher's Algorithm
+
+    def test_manachers_single_character_center(self):
+        s = Solution()
+
+        result = s.manachersLongestPalindrome("aba")
+        self.assertEqual(result, "aba")
+
+    def test_manachers_double_character_center(self):
+        s = Solution()
+
+        result = s.manachersLongestPalindrome("abba")
+        self.assertEqual(result, "abba")
+
+    def test_manachers_empty_string(self):
+        s = Solution()
+
+        result = s.manachersLongestPalindrome("")
+        self.assertEqual(result, "")
+
+    def test_manachers_one_char_string(self):
+        s = Solution()
+
+        result = s.manachersLongestPalindrome("a")
+        self.assertEqual(result, "a")
+
+    def test_manachers_one_char_palindrome(self):
+        s = Solution()
+
+        result = s.manachersLongestPalindrome("abc")
+        self.assertEqual(result, "a")
+
 
 if __name__ == "__main__":
     unittest.main()
