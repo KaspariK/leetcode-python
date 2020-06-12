5220fcd3dc1462b6223a1cbda163a9a1a739c590
Kevin Kaspari <KKASPARI@goremutual.ca>
2020-06-12 15:39:37 -0400

    Add XOR solution and tests for missing-number-268

--- missing-number-268/solution.py
+++ missing-number-268/solution.py
@@ -8,3 +8,25 @@ class Solution:
         # Gauss's formula for the sum of a series will give us the "expected" sum of a series of n length
         # Subtract the actual sum to find the missing value
         return (n * (n + 1) // 2) - sum(nums)
+
+    def missingNumberXOR(self, nums: List[int]) -> int:
+        # XOR has some properties that are really helpful in solving this
+        #
+        # Commutative: a ^ b = b ^ a so input order doesn't matter
+        # Associative: a ^ (b ^ c) = (a ^ b) ^ c so operations can be chained in anyorder
+        # Self inverse: a ^ a = 0 so any value XOR'd with itself returns zero
+        #
+        # Also, XOR helps avoid any potential overflow concerns (not a huge concern with Python, but still) as a result of summation
+
+        total_xor = 0
+        list_xor = 0
+
+        # XOR of all values in complete series
+        for i in range(1, len(nums) + 1):
+            total_xor ^= i
+
+        # XOR of all values in incomplete series
+        for num in nums:
+            list_xor ^= num
+
+        return total_xor ^ list_xor
--- missing-number-268/test_solution.py
+++ missing-number-268/test_solution.py
@@ -17,6 +17,20 @@ class TestSolution(unittest.TestCase):
 
         self.assertEqual(actual, 2)
 
+    def test_missingNumberXOR(self):
+        s = Solution()
+
+        actual = s.missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1])
+
+        self.assertEqual(actual, 8)
+
+    def test_missingNumberXOR_short(self):
+        s = Solution()
+
+        actual = s.missingNumber([3, 0, 1])
+
+        self.assertEqual(actual, 2)
+
 
 if __name__ == "__main__":
     unittest.main()
